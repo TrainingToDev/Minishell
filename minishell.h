aa5f125705d706067dd854a13a7111035a5ccb9f
Helitsanta Randrianarisoa <herandri@c1r4s1.42antananarivo.mg>
2024-10-31 15:20:46 +0300

    test_header

---
+++ minishell.h
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minishell.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: herandri <herandri@student.42antananarivo. +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/31 09:24:34 by herandri          #+#    #+#             */
+/*   Updated: 2024/10/31 12:00:22 by herandri         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+# ifndef MINISHELL_H
+# define MINISHELL_H
+
+#include "libft/libft.h"
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <readline/readline.h>
+#include <readline/history.h>
+
+
+typedef struct s_data_path
+{
+	char	**paths;
+	char	*path;
+	char	*full_path;
+	int		i;
+}			t_data_path;
+
+
+
+
+#endif
