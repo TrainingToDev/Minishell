7bdc4ced3fd9570678cec362b5c4a15a224155c2
herandri <andranheli@gmail.com>
2024-11-02 09:00:40 +0300

    add env

--- test_me/builtins_env.c
+++ test_me/builtins_env.c
@@ -6,8 +6,17 @@
 /*   By: herandri <herandri@student.42antananarivo. +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/01 10:42:13 by herandri          #+#    #+#             */
-/*   Updated: 2024/11/01 10:46:30 by herandri         ###   ########.fr       */
+/*   Updated: 2024/11/02 08:40:24 by herandri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
+
+void print_env(char **envp)
+{
+    while (*envp) 
+    {
+        printf("%s\n", *envp);
+        envp++;
+    }
+}
