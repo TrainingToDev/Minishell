f30c55cb053bcb8d381d3171cdbb5d85dbb35fc5
Helitsanta Randrianarisoa <herandri@c1r4s3.42antananarivo.mg>
2024-11-02 11:48:37 +0300

    update exit

--- test_me/builtins_exit.c
+++ test_me/builtins_exit.c
@@ -6,8 +6,16 @@
 /*   By: herandri <herandri@student.42antananarivo. +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/01 10:42:24 by herandri          #+#    #+#             */
-/*   Updated: 2024/11/01 10:46:34 by herandri         ###   ########.fr       */
+/*   Updated: 2024/11/02 11:43:29 by herandri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
+
+void exit_cmd(char *input)
+{
+    if (ft_strcmp(input, "exit") == 0)
+	{
+		free(input);
+	}
+}
