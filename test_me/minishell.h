cce403e3f048d0eba97a84bd65885368f084bc51
herandri <andranheli@gmail.com>
2024-11-12 04:02:09 +0300

    Data structure for minishell(v1)

--- test_me/minishell.h
+++ test_me/minishell.h
@@ -6,50 +6,131 @@
 /*   By: herandri <herandri@student.42antananarivo. +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/31 09:24:34 by herandri          #+#    #+#             */
-/*   Updated: 2024/11/02 16:21:59 by herandri         ###   ########.fr       */
+/*   Updated: 2024/11/12 04:00:11 by herandri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 # ifndef MINISHELL_H
 # define MINISHELL_H
 
-# include "libft/libft.h"
+# include "../libft/libft.h"
 # include <unistd.h>
-# include <fcntl.h>
-# include <stdio.h>
 # include <stdlib.h>
-#include <sys/wait.h>
+# include <stdio.h>
+# include <string.h>
+# include <signal.h>
 # include <readline/readline.h>
 # include <readline/history.h>
 # include <errno.h>
+# include <ctype.h>
+# include <sys/wait.h>
+# include <fcntl.h>
+# include <dirent.h>
+# include <sys/stat.h>
+# include <termios.h>
+# include <limits.h>
 
 # define RUN 1
-# define CMD_NOT_FOUND 127
 
-// struture proposal
-typedef struct s_data_path
+// color for prompt
+# define COLOR_RESET "\033[0m"
+# define COLOR_GREEN "\033[32m"
+# define COLOR_BLUE  "\033[34m"
+# define COLOR_RED   "\033[31m"
+
+
+// define only global for signal
+//here
+
+
+// struct for type of tokens
+typedef enum e_token_type
 {
-	char	**paths;
-	char	*path;
-	char	*full_path;
-	int		i;
-}			t_data_path;
+	TOKEN_WORD, // (cmd, args)
+	TOKEN_PIPE, // |
+	TOKEN_REDIRECT_IN, // <
+	TOKEN_REDIRECT_OUT, // >
+	TOKEN_APPEND, // >>
+	TOKEN_HEREDOC, // <<
+	TOKEN_AND, // &&
+	TOKEN_OR, // ||
+	TOKEN_LPAREN, // '('
+	TOKEN_RPAREN, // ')'
+	TOKEN_EOF,
+	TOKEN_UNKNOWN // not define
+}   t_token_type;
 
+// struct manage for tokenization (Lexical Analyser)
+typedef struct s_token
+{
+	char            *value;
+	t_token_type    type;
+	int             expand;
+	struct s_token  *next;
+}   t_token;
+
+// struct for type of redirections
+typedef enum e_redir_type
+{
+	REDIR_IN,        // '<'
+	REDIR_OUT,       // '>'
+	REDIR_APPEND,    // '>>'
+	REDIR_HEREDOC    // '<<'
+}   t_redir_type;
+
+// struct manage redirections
+typedef struct s_redirection
+{
+	t_redir_type             type;
+	char                     *filename;
+	struct s_redirection     *next;
+}   t_redirection;
+
+// struct for type AST
+typedef enum e_node_type
+{
+	NODE_COMMAND,
+	NODE_PIPE,
+	NODE_AND, // &&
+	NODE_OR, // ||
+	NODE_LOGICAL, // manage logical for cmd
+	NODE_SUBSHELL // manage brackets
+}   t_node_type;
+
+// struct for building AST
+typedef struct s_ast
+{
+	t_node_type       type;
+	struct s_command  *command;
+	t_token_type      operator;
+	struct s_ast      *left;
+	struct s_ast      *right;
+}   t_ast;
+
+// struct for cmd
+typedef struct s_command
+{
+	char                **argv;
+	int                 argc;
+	t_redirection       *redirs;
+}   t_command;
+
+// struct for environment variable
+typedef struct s_env_var
+{
+	char                *key;
+	char                *value;
+	struct s_env_var    *next;
+}   t_env_var;
+
+// struct for minishell
 typedef struct s_minishell
 {
-	char *name;
-	char *option;
-	char *arg;
-}	t_minishell;
+	t_env_var   *env_list;
+	int         last_exit_status;
+	int         interactive;
+}   t_minishell;
 
-
-typedef struct	s_cmd_list
-{
-	char				*data;
-	int					type;
-	struct s_cmd_list	*next;
-	
-}						t_cmd_list;
 
 
 // test function
