f2240c7dc3e27c960acc7d8b5732e229626d75f3
TrainingToDev <andranheli@gmail.com>
2025-02-10 16:10:11 +0300

    fixed msg error export

--- final/srcs/utils/utils_env.c
+++ final/srcs/utils/utils_env.c
@@ -38,6 +38,35 @@ void	free_env_list(t_env_var *env_list)
 	}
 }
 
+static int	export_arg_silent(t_minishell *shell, char *arg)
+{
+	char	*key;
+	char	*value;
+	char	*equal_sign;
+
+	equal_sign = ft_strchr(arg, '=');
+	if (equal_sign)
+	{
+		key = ft_substr(arg, 0, equal_sign - arg);
+		value = ft_strdup(equal_sign + 1);
+	}
+	else
+	{
+		key = ft_strdup(arg);
+		value = NULL;
+	}
+	if (!is_valid_id(key))
+	{
+		free(key);
+		free(value);
+		return (-1);
+	}
+	update_env(shell, key, value);
+	free(key);
+	free(value);
+	return (0);
+}
+
 void	fast_export(t_token *tokens, t_minishell *shell)
 {
 	t_token	*cur;
@@ -52,7 +81,7 @@ void	fast_export(t_token *tokens, t_minishell *shell)
 			next = cur->next;
 			while (next && next->type == TOKEN_WORD)
 			{
-				export_arg(shell, next->value);
+				export_arg_silent(shell, next->value);
 				next = next->next;
 			}
 		}
